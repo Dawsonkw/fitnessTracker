8fd7bc44343679046a34338d50a8ff7cf38db5a8
Dawsonkw <dawsonkw@gmail.com>
2025-04-03 16:04:06 -0600

    first push for fitness tracker app for more react

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-5xl font-bold text-red-600">Fitness Tracker</h1>
+    </>
+  );
+}
+
+export default App;
+
+// https://recharts.org/en-US
+// Use Recharts to create a chart to display fitness data
