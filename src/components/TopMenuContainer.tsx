997e5c6f457236d42cd0aa60ddd5605a39ea4ff7
Dawsonkw <dawsonkw@gmail.com>
2025-04-06 14:25:02 -0600

    vscode is doing something sketchy and im gonna have to resize this later but it aligns for now

--- src/components/TopMenuContainer.tsx
+++ src/components/TopMenuContainer.tsx
@@ -1,12 +1,8 @@
-interface TopMenuContainerProps {
-  items: string[];
-}
-
 function TopMenuContainer() {
   const items = ["Workouts", "Nutrition", "Sleep"];
 
   return (
-    <div className="bg-gray-300 flex align-top justify-center p-4 rounded-t-lg shadow-lg  fixed top-0 left-0 w-full z-10 mt-8 ml-8 mr-8">
+    <div className="bg-gray-300 flex align-top justify-center p-4 rounded-t-lg shadow-lg fixed w-[97.5%] top-0 left-0  z-10 mt-8 mx-8">
       <ul className="flex space-x-4">
         {items.map((item, index) => (
           <li key={index}>{item}</li>
