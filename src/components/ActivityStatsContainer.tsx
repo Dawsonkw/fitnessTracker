0a1ed81dc7d58db6511ead8e04364ec961e042d9
Dawsonkw <dawsonkw@gmail.com>
2025-04-08 15:50:50 -0600

    containers for styling / layout built out. gonna start hardcoding stuff in for placeholder for now

--- src/components/ActivityStatsContainer.tsx
+++ src/components/ActivityStatsContainer.tsx
@@ -1,5 +1,11 @@
 function ActivityStatsContainer() {
-  return <div></div>;
+  return (
+    <div>
+      <div className="bg-blue-200 flex w-[75vw] h-[40.7vh] justify-center items-center fixed top-175 left-0 z-0 mt-8 ml-8 mr-8 p-8 rounded-bl-lg">
+        <h1>Activity Stats</h1>
+      </div>
+    </div>
+  );
 }
 
 export default ActivityStatsContainer;
--- src/components/SidebarContainer.tsx
+++ src/components/SidebarContainer.tsx
@@ -4,7 +4,7 @@ import GoalsContainer from "./GoalsContainer";
 
 function SidebarContainer() {
   return (
-    <div>
+    <div className="bg-gray-800 flex flex-col w-[22.5vw] h-[90.75vh] justify-center items-center fixed top-14 right-0 z-0 mt-8 mr-8 p-8 rounded-br-lg">
       <UserInfoContainer />
       <JournalsContainer />
       <GoalsContainer />
