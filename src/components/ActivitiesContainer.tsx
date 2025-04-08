5081bc1351f39689de6faab2feaaa94e032d7325
Dawsonkw <dawsonkw@gmail.com>
2025-04-08 15:40:18 -0600

    building out more containers to design layout

--- src/components/ActivitiesContainer.tsx
+++ src/components/ActivitiesContainer.tsx
@@ -1,5 +1,11 @@
 function ActivitiesContainer() {
-  return <div></div>;
+  return (
+    <div>
+      <div className="bg-green-200 flex w-[75vw] h-[50vh] justify-center items-center fixed top-14 left-0 z-0 mt-8 ml-8 mr-8 p-8">
+        <h1>Activities</h1>
+      </div>
+    </div>
+  );
 }
 
 export default ActivitiesContainer;
--- src/components/TopMenuContainer.tsx
+++ src/components/TopMenuContainer.tsx
@@ -2,7 +2,7 @@ function TopMenuContainer() {
   const items = ["Workouts", "Nutrition", "Sleep"];
 
   return (
-    <div className="bg-gray-300 flex align-top justify-center p-4 rounded-t-lg shadow-lg fixed w-[97.5%] top-0 left-0  z-10 mt-8 mx-8">
+    <div className="bg-gray-300 border-b-2 border-gray-300 flex align-top justify-center p-4 rounded-t-lg fixed w-[97.5%] top-0 left-0  z-10 mt-8 mx-8">
       <ul className="flex space-x-4">
         {items.map((item, index) => (
           <li key={index}>{item}</li>
